1216d2df8aa6fe2c6d6467f7a51191833b711631
sueon kim <sekim@heronation.net>
2018-06-15 16:56:13 +0900

    1

--- src/main/java/net/heronation/zeyo/rest/service/common/CommonService.java
+++ src/main/java/net/heronation/zeyo/rest/service/common/CommonService.java
@@ -5,4 +5,6 @@ import java.util.Map;
 public interface CommonService {
  
 	Map<String,Object> dash_board_statistic();
+	
+	String send_sms(String p_from_phone,String p_to_phone,String p_msg);
 }
--- src/test/java/net/heronation/zeyo/rest/member/MemberRepositoryTest.java
+++ src/test/java/net/heronation/zeyo/rest/member/MemberRepositoryTest.java
@@ -23,12 +23,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.daou.BizSend;
+import com.daou.entity.SendMsgEntity;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQuery;
 
 import lombok.extern.slf4j.Slf4j;
+import net.heronation.zeyo.rest.constants.CommonConstants;
 import net.heronation.zeyo.rest.repository.brand.QBrand;
 import net.heronation.zeyo.rest.repository.company_no_history.QCompanyNoHistory;
 import net.heronation.zeyo.rest.repository.item.QItem;
@@ -47,7 +50,86 @@ public class MemberRepositoryTest {
 	
 	@Autowired
 	EntityManager entityManager;
+
 	
+	@Test 
+	public void send_sms_test() {
+		
+		this.send_sms("01028063412","메세지 전송 테스트");
+	}
+	public String send_sms(String p_to_phone,String p_msg) {
+		
+
+		BizSend bs        = new BizSend();    // 메시지 전송 클래스
+		SendMsgEntity sme = null;             // 메시지 클래스
+
+		/* Console 로그 설정 */
+		// Console 에서 로그를 확인할 경우 설정
+		bs.setLogEnabled(true);
+
+		/* 서버 연결 시작 & 인증 */
+		// ip   = biz.ppurio.com = 211.189.43.25
+		// port = 5000 / 15001 /18100
+		bs.doBegin("biz.ppurio.com", 5000, "heronation", "ironman4$$");
+
+		/* 전송할 파일 경로 설정 */
+		// 전송할 파일이 있을 경우 설정
+		// ex. FAX, PHONE, MMS 등
+//		bs.setFilePath("./spool");
+
+		/* 블랙리스트 파일 경로 설정 */
+		// 블랙리스트를 사용할 경우 설정
+//		bs.setBlkPath("./blk");
+
+		/* PING-PONG */
+		// 장시간 연결하여 메시지를 전송할 경우 연결이 끊어지지 않기 위해 실행
+		// 이전 호출 시점으로부터 30초 이상 지난 경우에만 PING 을 전송하도록 함수내  정의되어 있음
+		// 실행하지 않아도 메시지 전송에는 영향을 미치지 않으나 안전한 연결을 위해 주기적으로 실행하길 권장
+//		boolean bool = bs.sendPing();
+//		System.out.println("Result of a Ping=" + bool);
+
+		/* 리포트 재요청 */
+		// msgid 에 해당하는 메시지의 리포트가 시간이 지나도 오지 않는 경우 실행
+//		bool = bs.reconfirmReport("");    // @param msgid    다우기술의 서버에서 정의한 message id
+//		System.out.println("Result of Reconfirming=" + bool);
+
+		/* 메시지 정의 */
+		// 다음의 setter 를 사용하여 필요한 정보를 정의
+		// ex. SMS 의 경우, MSG_TYPE, DEST_PHONE, SEND_PHONE, MSG_BODY 정보를 정의
+		sme = new SendMsgEntity();
+		sme.setCMID      ("");    // 데이터 id
+		sme.setMSG_TYPE  (0 );    // 데이터 타입 (SMS 0 / WAP 1 / FAX 2 / PHONE 3 / SMS_INBOUND 4 / MMS 5)
+		sme.setSEND_TIME ("");    // 발송 (예약) 시간 (Unix Time, 정의하지 않을 경우 즉시 발송)
+		sme.setDEST_PHONE(p_to_phone);    // 받는 사람 전화 번호
+		sme.setDEST_NAME ("");    // 받는 사람 이름
+		sme.setSEND_PHONE("01038863419");    // 보내는 사람 전화 번호
+		sme.setSEND_NAME ("");    // 보내는 사람 이름
+		sme.setSUBJECT   ("");    // (FAX/MMS) 제목, (SMS_INBOUND) 데이터 내용
+		sme.setMSG_BODY  (p_msg);    // 데이터 내용
+		sme.setWAP_URL   ("");    // (WAP) URL 주소
+		sme.setCOVER_FLAG(0 );    // (FAX) 표지 발송 옵션
+		sme.setSMS_FLAG  (0 );    // (PHONE) PHONE 실패 시 문자 전송 옵션
+		sme.setREPLY_FLAG(0 );    // (PHONE) 응답 받기 선택
+		sme.setRETRY_CNT (0 );    // (FAX/PHONE) 재시도 회수 (5~10분 간격: 최대 3회)
+		sme.setFAX_FILE  ("");    // (PHONE/FAX/MMS) 파일 전송시 파일 이름
+		sme.setVXML_FILE ("");    // (PHONE) 음성 시나리오 파일 이름
+
+		/* 메시지 전송 */
+		try {
+			String msgid = "";
+			msgid = bs.sendMsg(sme);    // @param  sme       SendMsgEntity
+			                            // @retrun String    다우기술의 서버에서 정의한 message id
+
+			System.out.println("msgid=" + msgid);
+		} catch (Exception ex) {
+			System.out.println("Failed to Send a Msg " + ex.getMessage());
+		}
+
+		/* 서버 연결 종료 */
+		bs.doEnd();
+		
+		return CommonConstants.OK;
+	}
 	
 	@Test
 	@Ignore
@@ -237,6 +319,7 @@ public class MemberRepositoryTest {
 	
 	@SuppressWarnings("unchecked")
 	@Test 
+	@Ignore
 	public void statistic_query() {
 		
 		
