860e760390d7414ee0cff801fba798fe47b72a22
sueon kim <sekim@heronation.net>
2018-08-13 11:43:01 +0900

    1

--- dynamic_model_util/test.js
+++ dynamic_model_util/test.js
@@ -1,6 +1,6 @@
-var target = SVG.get("target");  
+var target = SVG.get("target");
 var length = target.length();
-var rect  =  SVG.get("marker");  
+var rect = SVG.get("marker");
 
 var cursor = 0;
 var constants_fourth = 0.1;
@@ -8,32 +8,30 @@ var constants_third = 0.01;
 var constants_double = 0.001;
 var constants = 0.0001;
 
-var move_rect = function(p_step){
+var move_rect = function(p_step) {
 	cursor += p_step;
 
 	var p = target.pointAt(cursor * length);
 	rect.center(p.x, p.y);
 };
 
-$("#btn_third_back").bind("click", function() { 
-	move_rect(-constants_fourth); 
+$("#btn_third_back").bind("click", function() {
+	move_rect(-constants_fourth);
 });
 
-
-$("#btn_third_back").bind("click", function() { 
-	move_rect(-constants_third); 
+$("#btn_third_back").bind("click", function() {
+	move_rect(-constants_third);
 });
 
-$("#btn_double_back").bind("click", function() { 
-	move_rect(-constants_double); 
+$("#btn_double_back").bind("click", function() {
+	move_rect(-constants_double);
 });
 $("#btn_back").bind("click", function() {
 	move_rect(-constants);
 });
 
-
 $("#btn_forth").bind("click", function() {
-	move_rect(constants); 
+	move_rect(constants);
 });
 $("#btn_double_forth").bind("click", function() {
 	move_rect(constants_double);
